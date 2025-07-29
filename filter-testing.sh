b33208b60b4d718f088c251b2ac9126c1cfb2a76
Dalila Pinto <dalila.pinto@mssm.edu>
2025-07-29 11:29:12 -0400

    Maintain a temporary script to test the new filtering steps before implementing them in the pipeline

---
+++ filter-testing.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# 29.07.2025 10:13:11 EDT
+
+
+cd /sc/arion/scratch/pintod02/isopropeller-collapse-test/04_isoPropeller-merge
+
+# Monoexon tss filter
+~/opt/isoPropeller-collapse/workflow/scripts/filter_monoexon-tss-overlap.py \
+   --isoform_gtf   ISOP_depth-gt1.gtf \
+   --reftss_bed    /sc/arion/work/pintod02/opt/isoseq_pipeline/data/combined_cage_Pitt-Fantom5-119FrontalLob_refTSS3.3_refseq_gencode_extended.merged.sorted_chr.bed \
+   --out_gtf       filter-test-monoexon-tss.gtf \
+   --out_ids       filter-test-monoexon-tss.ids \
+   --max_distance  100 \
+   --genome_index  /sc/arion/projects/pintod02c/reference-databases/hg38-v41-ERCC/GRCh38.primary_assembly.genome.fa.fai
